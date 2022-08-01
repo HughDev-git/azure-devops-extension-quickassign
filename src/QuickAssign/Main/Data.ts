ec4fcb1bb0f2516e79005769763c6d41b847ccb0
HughDev-git <65168159+HughDev-git@users.noreply.github.com>
2022-08-01 16:08:02 -0400

    .
    .

--- src/QuickAssign/Main/Data.ts
+++ src/QuickAssign/Main/Data.ts
@@ -1,40 +1,67 @@
-export interface ITaskItem<T = {}> {
-    areapathfull: string;
-    areapathshort: string;
-    title: string;
-    id: number;
-    url: string;
-    urlforlinking: string;
+import { IdentityRef } from "azure-devops-extension-api/WebApi";
+import { ISimpleListCell } from "azure-devops-ui/List";
+import {
+  ColumnMore,
+  ColumnSelect,
+  ISimpleTableCell,
+  ITableColumn,
+  renderSimpleCell,
+  TableColumnLayout
+} from "azure-devops-ui/Table";
+import { ObservableValue } from "azure-devops-ui/Core/Observable";
+import { getClient } from "azure-devops-extension-api";
+import { CoreRestClient } from "azure-devops-extension-api/Core/CoreClient";
+
+export interface ITableItem extends ISimpleTableCell {
+  displayName: string;
+  descriptor: string;
+}
+
+export interface IUserItem<T = {}> {
+    identity: IdentityRef
+    name: string
+}
+
+export const fixedColumns = [
+   createColumnSelect<ITableItem>(),
+  {
+    columnLayout: TableColumnLayout.singleLinePrefix,
+    id: "displayName",
+    name: "Name",
+    readonly: true,
+    renderCell: renderSimpleCell,
+    width: new ObservableValue(-30)
   }
+  // new ColumnMore(() => {
+  //   return {
+  //     id: "sub-menu",
+  //     items: [{ id: "assign", text: "Assign" }]
+  //   };
+  // })
+];
+
+let usersInGroup = main();
+
+async function main(){
+  const users = await getMembersOfTeam()
+  return users
+
+}
+
+export const usersAssigned = usersInGroup
+
+ async function getMembersOfTeam(){
+  const client = getClient(CoreRestClient)
+  let users = await client.getTeamMembersWithExtendedProperties("Master Template","My Test Team")
+  return users
+}
+function createColumnSelect<T>(): ITableColumn<T> {
+  /* workaround incompatible types */
+  return new ColumnSelect() as unknown as ITableColumn<T>
+}
   
-  export const MSStoryData: ITaskItem[] = [
-    // {
-    //   description: "Build, test, package, or publish a dotnet application",
-    //   name: ".NET Core"
-    // },
-    // {
-    //   description:
-    //     "Acquires a specific version of .NET Core from internal installer",
-    //   name: ".NET Core Tool Installer"
-    // },
-    // {
-    //   description: "Sign and align Android APK Files",
-    //   name: "Android Signing"
-    // },
-    // {
-    //   description: "Distribute app builds to testers and users via AppCenter",
-    //   name: "App Center Distribute"
-    // },
-    // {
-    //   description: "Test app packages with App Center",
-    //   name: "App Center Test"
-    // },
-    // {
-    //   description: "Archive files using compression formats",
-    //   name: "Archive Files"
-    // },
-    // {
-    //   description: "Update Azure App Services on Windows, Web Applications, etc.",
-    //   name: "Azure App Service Deploy"
-    // }
+  export const MSStoryData: IUserItem[] = [
+  ];
+
+  export const rawTableItems: ITableItem[] = [
   ];
