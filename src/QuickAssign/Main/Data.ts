12dc64f97e693d394655aecab793bff563b280e4
HughDev-git <65168159+HughDev-git@users.noreply.github.com>
2022-08-01 21:09:00 -0400

    .
    .

--- src/QuickAssign/Main/Data.ts
+++ src/QuickAssign/Main/Data.ts
@@ -11,10 +11,20 @@ import {
 import { ObservableValue } from "azure-devops-ui/Core/Observable";
 import { getClient } from "azure-devops-extension-api";
 import { CoreRestClient } from "azure-devops-extension-api/Core/CoreClient";
+import {
+  IWorkItemFormService,
+  WorkItemExpand,
+  WorkItemRelation,
+  WorkItemTrackingRestClient,
+  WorkItemTrackingServiceIds,
+} from "azure-devops-extension-api/WorkItemTracking";
+import * as SDK from "azure-devops-extension-sdk";
 
-export interface ITableItem extends ISimpleTableCell {
+export interface ITableItem extends ISimpleListCell {
   displayName: string;
   descriptor: string;
+  isAssignedActivity: boolean;
+  linkItemInterface: WorkItemRelation[] 
 }
 
 export interface IUserItem<T = {}> {
@@ -40,15 +50,46 @@ export const fixedColumns = [
   // })
 ];
 
-let usersInGroup = main();
+let usersInGroup = retrieveUsers();
+let childActivities = retrieveChildItems();
 
-async function main(){
+async function retrieveUsers(){
   const users = await getMembersOfTeam()
   return users
+}
 
+async function retrieveChildItems(){
+  const users = await getMembersOfTeam()
+  return users
+}
+
+async function getChildItems(){
+  const workItemFormService = await SDK.getService<IWorkItemFormService>(
+    WorkItemTrackingServiceIds.WorkItemFormService
+  );
+  // const organization = await SDK.getHost()
+  // const project = await Project
+  let placeholder = new Array<ITableItem>()
+  const client = getClient(WorkItemTrackingRestClient);
+  let relations = await workItemFormService.getWorkItemRelations();
+  for (let item of relations){
+    console.log("Attributes: "+item.attributes+" ||| Link Type: "+item.rel+" ||| URL: "+item.url)
+    if(item.rel == "System.LinkTypes.Hierarchy-Forward"){
+      //Get the id from end of string
+      var matches : number;
+      matches = parseInt(item.url.match(/\d+$/)?.toString()||"")
+      console.log(matches);
+      client.getWorkItemTypeFieldsWithReferences
+      let workitem = client.getWorkItem(matches, undefined, undefined, new Date(), WorkItemExpand.Relations)
+      let assignedTo = (await workitem).fields["System.AssignedTo"]
+      console.log(assignedTo)
+      return assignedTo
+}
+}
 }
 
 export const usersAssigned = usersInGroup
+export const getChildItems1 = childActivities
 
  async function getMembersOfTeam(){
   const client = getClient(CoreRestClient)
